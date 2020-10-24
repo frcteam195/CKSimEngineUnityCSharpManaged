a8f8c24e3ad64a46998ed0cd7a8c1595d2e86355
Robert Hilton <robert.a.hilton.jr@gmail.com>
2020-10-24 04:23:11 -0400

    Add a test to get and set some data to make sure connection works

---
+++ CKSimEngineManagedPluginUnityTester/Assets/Scripts/TestValues.cs
@@ -0,0 +1,25 @@
+﻿using CKSimEngineManagedPlugin;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestValues : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        CKSimNetCode.Instance.SetAccelerometer(0, 0.4f);
+        Debug.Log(CKSimNetCode.Instance.GetMotor(0));
+    }
+}
